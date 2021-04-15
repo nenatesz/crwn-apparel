acc45226b95b86b5853637b44b7316a0d2adfed2
Theresa Anyadike <nnennaanyadike@gmail.com>
2021-04-15 12:27:19 +0100

    updating application with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 
 import './cart-icon.styles.scss';
 
-const CartIcon = ({toggleCartHidden}) => (
+const CartIcon = ({toggleCartHidden, itemCount }) => (
     <div className='cart-icon'>
         <ShoppingIcon className='shopping-icon'  onClick={toggleCartHidden}/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCount}</span>
         </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);  
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);  
 
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from 'reselect';
+
+// using createselector makes this selector it becomes a memoir selector
+
+// this is an input selector that returns only a piece of the state
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    (cartItems) => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+    )
+);
